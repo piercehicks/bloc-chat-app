83211a274decfc7f6316fff261e51296d740a38d
Pierce <phicks21@gmail.com>
2019-01-27 17:17:31 -0500

    tutor attempt 1

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
 class User extends Component {
+  constructor(props){
+    super(props);
+
+    this.setState = {
+      
+    }
+
+  }
 
 
 handleSignIn(){
   const provider = new this.props.firebase.auth.GoogleAuthProvider();
-  this.props.firebase.auth().signInWithPopup( provider );
+ this.props.firebase.auth().signInWithPopup(provider).then((result) => {
+   const user = result.user;
+   this.props.setUser(user);
+ });
 
 };
 
